57f3d494684a322bbb122c2387f2394b3aaf5736
JoseCutileiro <jos3ndo@gmail.com>
2024-01-28 08:45:32 +0000

    testing fottest

---
+++ Forms_Csharp/TutorialsSolution/GeographyLibrary/Country.cs
@@ -0,0 +1,32 @@
+﻿namespace GeographyLibrary
+{
+    public class Country
+    {
+        private string name;
+        private int population;
+
+        public Country(string name, int population)
+        {
+            this.name = name;
+            this.population = population;
+        }
+
+        public Country()
+        {
+            return;
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+
+        }
+
+        public int Population
+        {
+            get { return population; }
+            set { population = value; }
+        }
+    }
+}
---
+++ Forms_Csharp/TutorialsSolution/Tutorial2Solution/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using GeographyLibrary;
+
+namespace Tutorial2Solution
+{
+    public partial class Form1 : Form
+    {
+
+        private List<Country> countryList;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            // INIT
+
+            countryList = new List<Country>();
+
+            Country c1 = new Country();
+            c1.Name = "Portugal";
+            c1.Population = 11;
+
+            Country c2 = new Country();
+            c2.Name = "Sweden";
+            c2.Population = 10;
+
+            countryList.Add(c1);
+            countryList.Add(c2);
+
+            button2.Enabled = true;
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // SORT
+            countryList.Sort((a, b) => a.Population.CompareTo(b.Population));
+            listBox1.Items.Clear();
+
+            foreach (Country c in countryList)
+            {
+                string info = "[" + c.Name + "]: Population " + c.Population + " millions";
+                listBox1.Items.Add(info);
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
